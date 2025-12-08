3d56d21c63f739173630415b584f2f0c632b46ee
Edunoh <eedunoh@gmail.com>
2025-12-08 19:16:48 +0000

    Created and updated a new table - patient_and_procedure.sql

---
+++ models/patient_and_procedures.sql
@@ -0,0 +1,13 @@
+Select 
+pa.prefix
+, pa.first
+, pa.last
+, pa.marital
+, pa.id As patient_ids
+, pa.race
+, po.description
+, po.start_time As procedure_start
+, po.stop_time As procedure_stop
+From raw.patient_records.patients pa
+Left Join raw.patient_records.procedures po
+On pa.id = po.patient
